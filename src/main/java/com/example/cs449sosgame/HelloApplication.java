292d012db0658c8887e782f3a78b21b9e6c03034
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2021-09-17 20:20:45 -0500

    Basic gui

--- src/main/java/com/example/cs449sosgame/HelloApplication.java
+++ src/main/java/com/example/cs449sosgame/HelloApplication.java
@@ -12,8 +12,12 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setWidth(600);
+        stage.setHeight(550);
+
         stage.show();
     }
 
