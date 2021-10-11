09add4677ce6d84763866bf516b8bc9966a4176f
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2021-10-11 14:55:56 -0500

    Create SOSGame.java

---
+++ src/main/java/com/example/cs449sosgame/SOSGame.java
@@ -0,0 +1,36 @@
+package com.example.cs449sosgame;
+
+import javafx.scene.control.Label;
+
+public class SOSGame {
+    private char[][] board;
+    private char activePlayer;
+    private Label showActivePlayer;
+    public SOSGame(int size)
+    {
+        activePlayer = 'S';
+        board = new char[size][size];
+
+    }
+
+    public void switchActivePlayer()
+    {
+        if (activePlayer == 'S')
+            activePlayer = 'O';
+        else activePlayer = 'S';
+
+    }
+    public void blockInput()
+    {
+
+    }
+
+
+
+    // TODO:
+    public void checkIfGameEnds ()
+    {
+
+
+    }
+}
