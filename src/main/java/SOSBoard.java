30ad7154105ee536491ebe25a6d737acfcc590ac
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2021-11-05 17:29:30 -0500

    CheckWin now works for S moves

--- src/main/java/SOSBoard.java
+++ src/main/java/SOSBoard.java
@@ -1,18 +1,21 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class SOSBoard{
     public enum Cell {EMPTY, S, O};
-    //private Cell[][] grid;
     protected char[][] grid;
     public enum GameState {PLAYING, DRAW, S_WON, O_WON};
     public GameState state;
-    //public Tile[][] grid;
     private int boardSize;
     private char activePlayer;
-
+    private int sPlayerPoints;
+    private int oPlayerPoints;
+    private int pointsNeededToWin = 1;
+    private int totalMoves;
 
     public SOSBoard(int size)
     {
+        totalMoves = 0;
         activePlayer = 'S';
         boardSize = size;
         state = GameState.PLAYING;
@@ -22,44 +25,144 @@ public class SOSBoard{
                 grid[i][j] = ' ';
 
     }
-    public GameState CheckWin(int x, int y)
-    {
-
-        // check column
-        for (int i = 0; i < grid.length; i++)
-        {
-            if(grid[x][i] != getActivePlayer())
-                break;
-            if(i == i-1 && i == grid.length)
-            {
-                // win
-            }
-
-        }
-        // check row
-        for (int i = 0; i < grid.length; i++)
-        {
-            if(grid[i][y] != getActivePlayer())
-                break;
-            if(i == i-1 && i == grid.length)
-            {
-                // win
-            }
-        }
-
-        // check diagonal
 
 
-        return GameState.PLAYING;
+    public GameState updateState(){
+        int maximumPossibleMoves = grid.length * grid.length;
+        if (sPlayerPoints >= pointsNeededToWin)
+            state = GameState.S_WON;
+        else if (oPlayerPoints >= pointsNeededToWin)
+            state = GameState.O_WON;
+        else if (totalMoves > maximumPossibleMoves)
+            state = GameState.DRAW;
+        else state = GameState.PLAYING;
+        return state;
+
+
     }
+
+    public int checkSOS(int row, int col)
+    {
+        char turn = getActivePlayer();
+
+        //char adjacentToken = (turn == 'S') ? 'O' : 'S';
+        //int offset = (turn == 'O') ? '1' : '0';
+        boolean sosFound = false;
+        int offset = 0;
+        int pointsThisTurn = 0;
+        if (turn == 'O' && grid[row][col] != 'O' )
+            pointsThisTurn += 0;
+        if (turn == 'O' && grid[row][col] == 'O' && row >0) {
+            pointsThisTurn += checkSOS(row-1, col);
+            //pointsThisTurn += checkSOS(row+1, col);
+
+        }
+        if (turn == 'O' && grid[row][col] == 'O' && col >0) {
+            pointsThisTurn += checkSOS(row, col-1);
+           // pointsThisTurn += checkSOS(row, col+1);
+
+        }
+
+
+        if (turn == 'O' && grid[row][col] == 'O'  && row > 0 && col > 0 )
+        {
+            pointsThisTurn+= checkSOS(row+1, col+1);
+        }
+
+
+
+        // up bound check
+
+        if (row +2 < grid.length)
+        {
+            if (turn == 'O')
+            {
+
+            }
+            if (grid[row-offset][col] == 'S'
+                    && grid[row-offset+1][col] == 'O'
+                    && grid[row-offset+2][col] == 'S')
+            {
+                sosFound = true;
+                pointsThisTurn += 1;
+            }
+
+
+
+            if (col +2 < grid.length)
+                if (grid[row-offset][col] == 'S'
+                        && grid[row-offset+1][col+1] == 'O'
+                        && grid[row-offset+2][col+2] == 'S')
+                {
+                    sosFound = true;
+                    pointsThisTurn += 1;
+                }
+                    sosFound = true;
+        }
+        // down bound check
+        if (row -2 >= 0 )
+        {
+            if (grid[row-offset][col] == 'S'
+                    && grid[row-offset-1][col] == 'O'
+                    && grid[row-offset-2][col] == 'S')
+            {
+                sosFound = true;
+                pointsThisTurn += 1;
+            }
+            if (col -2 >= 0)
+                if (grid[row-offset][col] == 'S'
+                        && grid[row-offset-1][col-1] == 'O'
+                        && grid[row-offset-2][col-2] == 'S')
+                {
+                    sosFound = true;
+                    pointsThisTurn += 1;
+                }
+
+
+        }
+        // right bound check
+         if (col + 2 < grid.length) {
+            if (grid[row][col-offset] == 'S'
+                    && grid[row][col-offset +1] == 'O'
+                    && grid[row][col-offset +2] == 'S')
+            {
+                sosFound = true;
+                pointsThisTurn += 1;
+            }
+        }
+        // left bound check
+         if (col - 2 >= 0)
+        {
+            if (grid[row][col-offset] == 'S'
+                    && grid[row][col-offset -1] == 'O'
+                    && grid[row][col-offset -2] == 'S')
+            {
+                sosFound = true;
+                pointsThisTurn += 1;
+            }
+
+
+        }
+
+        if (grid[row][col] == 'S')
+            sPlayerPoints += pointsThisTurn;
+        else if (grid[row][col] == 'O')
+            oPlayerPoints += pointsThisTurn;
+
+        return pointsThisTurn;
+    }
+
     public void makeMove(int row, int column)
     {
         if ((row >=0 && row < boardSize && column >= 0 && column < boardSize) && grid[row][column] == ' ')
         {
+            checkSOS(row, column);
+
             if (activePlayer == 'S')
             {
                 grid[row][column] = 'S';
-                if (CheckWin(row, column) == GameState.S_WON)
+                //checkSOS(row, column, activePlayer);
+                //if (CheckWin(row, column) == GameState.S_WON)
                 {
                     //TODO: report win
                 }
@@ -69,7 +172,8 @@ public class SOSBoard{
             else if (activePlayer == 'O')
             {
                 grid[row][column] = 'O' ;
-                if (CheckWin(row, column) == GameState.O_WON)
+                //checkSOS(row, column, activePlayer);
+             //   if (CheckWin(row, column) == GameState.O_WON)
                 {
                     //TODO: report win
                 }
@@ -99,6 +203,14 @@ public class SOSBoard{
 
 
         return cell;
+    }
+    public int getOplayerPoints()
+    {
+        return oPlayerPoints;
+    }
+    public int getSplayerPoints()
+    {
+        return sPlayerPoints;
     }
     public void switchActivePlayer()
     {
