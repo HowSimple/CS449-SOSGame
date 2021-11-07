8dfdaeff03d7812d69257c6c4f1174bd04ef7850
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2021-11-07 14:54:20 -0600

    Update GeneralGameBoard.java

--- src/main/java/GeneralGameBoard.java
+++ src/main/java/GeneralGameBoard.java
@@ -1,7 +1,20 @@
 public class GeneralGameBoard extends SOSBoard {
-    GeneralGameBoard(int size)
-    {
+
+    public GeneralGameBoard(int size) {
         super(size);
+    }
+
+    public void updateState(){
+        int maximumPossibleMoves = grid.length * grid.length;
+
+        if (movesThisGame < maximumPossibleMoves)
+            state = GameState.PLAYING;
+        else if (sPlayerPoints > oPlayerPoints)
+            state = GameState.S_WON;
+        else if (oPlayerPoints > sPlayerPoints)
+            state = GameState.O_WON;
+        else
+            state = GameState.DRAW;
 
     }
 }
