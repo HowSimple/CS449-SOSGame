f1fe8d4942721cf1a851dababd6c9cbfe446b600
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2021-11-07 14:54:29 -0600

    Update SimpleGameBoard.java

--- src/main/java/SimpleGameBoard.java
+++ src/main/java/SimpleGameBoard.java
@@ -6,10 +6,16 @@ public class SimpleGameBoard extends SOSBoard{
     SimpleGameBoard(int size)
     {
         super(size);
-        pointsNeededToWin = 1;
-
     }
-
-
-
+    public void updateState(){
+        int  pointsNeededToWin = 1;
+        int maximumPossibleMoves = grid.length * grid.length;
+        if (sPlayerPoints >= pointsNeededToWin)
+            state = GameState.S_WON;
+        else if (oPlayerPoints >= pointsNeededToWin)
+            state = GameState.O_WON;
+        else if (movesThisGame > maximumPossibleMoves)
+            state = GameState.DRAW;
+        else state = GameState.PLAYING;
+    }
 }
